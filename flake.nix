c2090a069d3363be7587728b04e0b8df947e8e37
liuhh666233 <liuxiaobo666233@gmail.com>
2021-08-25 17:28:54 +0800

    Adjust nix shell.

--- flake.nix
+++ flake.nix
@@ -2,49 +2,33 @@
 
   description = "All of our deployment, period";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
-    };
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05"; };
 
-  outputs = { self, nixpkgs, ... }@inputs: 
-  let 
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-  in 
-    {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in {
       nixosConfigurations = {
-        
-        lxb = nixpkgs.lib.nixosSystem rec{
-          inherit system;
-          modules = [
-            ./machines/lxb
-          ];
-        };
 
-        demo-vm = nixpkgs.lib.nixosSystem rec{
+        lxb = nixpkgs.lib.nixosSystem rec {
           inherit system;
-          modules = [
-            ./machines/demo-vm
-          ];
+          modules = [ ./machines/lxb ];
         };
-
       };
 
       devShell."${system}" = pkgs.mkShell rec {
-        name = "nix-demo-env";
-        
-        buildInputs = with pkgs; [
-          (python38.withPackages(ps: with ps; [ jupyter jupyter_core notebook ipython ipykernel systemd autopep8 pip ]))
-        ];
+        name = "nix-test";
+
+        buildInputs = with pkgs; [ pkgs.nixosTest ./login.nix ];
 
         shellHook = ''
           export PS1="$(echo -e '\uf3e2') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
         '';
       };
-
 
     };
 }
