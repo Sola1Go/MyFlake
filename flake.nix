840998c09e224f2319708ae0e94a3c76b2c20926
lyc <30356570@qq.com>
2022-09-30 09:54:40 +0800

    tmp

--- flake.nix
+++ flake.nix
@@ -3,48 +3,30 @@
   description = "All of our deployment, period";
 
   inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
 
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
-
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+    utils.inputs.nixpkgs.follows = "nixpkgs";
 
     home-manager = {
       url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-      pkgs-unstable = import nixpkgs-unstable {
-        inherit system;
-        config.allowUnfree = true;
-      };
-    in {
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
       nixosConfigurations = {
 
-        lyc = nixpkgs.lib.nixosSystem rec {
-          inherit system;
-          modules = [
-            ({
-              nixpkgs.overlays = [
-                (final: prev: {
-                  vscode-latest = pkgs-unstable.vscode;
-                })
-              ];
-            })
-            ./machines/lyc
-          ];
-        };
+
       };
 
       homeConfigurations.lyc = home-manager.lib.homeManagerConfiguration {
-        pkgs = pkgs-unstable;
+        inherit pkgs;
 
         modules = [
           ./home/user-lyc.nix
@@ -52,5 +34,7 @@
         ];
 
       };
+
+
     };
 }
